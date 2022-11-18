56b40c067818e373dcf20e8dccc77d46a34f90bd
Samir70 <sbetmouni@talktalk.net>
2022-11-18 12:29:01 +0000

    improve formatting

--- lib/music_tracker.rb
+++ lib/music_tracker.rb
@@ -1,25 +1,25 @@
 class MusicTracker
-    def initialize #takes no arguments
-      @music_list = [] #an array of hash objects
+  def initialize #takes no arguments
+    @music_list = [] #an array of hash objects
+  end
+
+  def add(title, artist)
+    if title.empty?
+      fail "Error: Title cannot be empty string."
     end
-    
-    def add(title, artist)
-        if title.empty?
-            fail "Error: Title cannot be empty string."
-        end
-        if artist.empty?
-            fail "Error: Artist cannot be empty string."
-        end
-        track = {}
-        track["Title"]=title
-        track["Artist"]=artist
-        @music_list << track
+    if artist.empty?
+      fail "Error: Artist cannot be empty string."
+    end
+    track = {}
+    track["Title"] = title
+    track["Artist"] = artist
+    @music_list << track
     #title is a string
     #artist is a string
     #title and artist are values within a hash
-    end
-    
-    def list_tracks() #takes no arguments
-        return @music_list
-    end
+  end
+
+  def list_tracks() #takes no arguments
+    return @music_list
+  end
 end
